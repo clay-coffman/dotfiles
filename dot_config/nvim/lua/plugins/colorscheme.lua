57bbf729ed01c099bbc8f1f9e6ac02988c69376c
Clay Coffman <claymcoffman@gmail.com>
2025-09-15 13:37:41 -0700

    Add .config/nvim/LICENSE Add .config/nvim/README.md Add .config/nvim/.gitignore Add .config/nvim/.neoconf.json Add .config/nvim/init.lua Add .config/nvim/lazy-lock.json Add .config/nvim/lazyvim.json Add .config/nvim/lua/config/autocmds.lua Add .config/nvim/lua/config/keymaps.lua Add .config/nvim/lua/config/lazy.lua Add .config/nvim/lua/config/options.lua Add .config/nvim/lua/plugins/blink.lua Add .config/nvim/lua/plugins/bufferline.lua Add .config/nvim/lua/plugins/colorscheme.lua Add .config/nvim/lua/plugins/conform.lua Add .config/nvim/lua/plugins/example.lua Add .config/nvim/lua/plugins/mini-align.lua Add .config/nvim/lua/plugins/nvim-lint.lua Add .config/nvim/lua/plugins/snacks.lua Add .config/nvim/lua/plugins/vim-tmux-navigator.lua Add .config/nvim/stylua.toml

---
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    opts = {
+      flavour = "auto",
+      dim_inactive = {
+        enabled = true,
+        shade = "dark",
+        percentage = 0.15,
+        auto_integrations = true,
+      },
+    },
+  },
+}
---
+++ dot_config/nvim/lua/plugins/mini-align.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "echasnovski/mini.align",
+    opts = {},
+    keys = {
+      { "ga", mode = { "n", "v" }, desc = "start align" },
+      { "gA", mode = { "n", "v" }, desc = "start align (with preview)" },
+    },
+  },
+}
