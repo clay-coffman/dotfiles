deff74e386f9bd14655e71db075b9a14866950d9
Clay Coffman <claymcoffman@gmail.com>
2025-09-17 13:02:21 -0700

    some opts

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -2,4 +2,10 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- use prettier config file in $HOME
 vim.g.lazyvim_prettier_needs_config = true
+
+local opt = vim.opt
+
+opt.conceallevel = 1 -- conceallevel relevant for markdown
+opt.tabstop = 2 -- number of spaces tabs count for
