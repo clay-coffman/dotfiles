e607a00ea9b0315677c1c12a9ae36305b866ba22
Clay Coffman <claymcoffman@gmail.com>
2025-09-20 15:56:24 -0500

    Update .config/kitty/kitty.conf Add .config/nvim/lua/plugins/colorscheme.bak Update .config/nvim/lua/plugins/colorscheme.lua Update .config/starship.toml

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,21 +1,12 @@
 return {
+  -- add gruvbox
+  { "altercation/vim-colors-solarized" },
+
+  -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin",
-    },
-  },
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    opts = {
-      flavour = "auto",
-      dim_inactive = {
-        enabled = true,
-        shade = "dark",
-        percentage = 0.15,
-        auto_integrations = true,
-      },
+      colorscheme = "solarized",
     },
   },
 }
