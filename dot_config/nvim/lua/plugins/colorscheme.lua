2ebaf06c246de749da3421f3e9e70d70e3ad3437
Clay Coffman <claymcoffman@gmail.com>
2025-09-20 21:34:13 -0500

    Update .config/kitty/kitty.conf Update .config/nvim/lua/plugins/colorscheme.lua Update .tmux.conf Change attributes of .config/nvim/lua/plugins/bufferline.bak Change attributes of .config/nvim/lua/plugins/codecompanion.bak

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,26 +1,18 @@
 return {
   {
-    "LazyVim/LazyVim",
+    "folke/tokyonight.nvim",
     opts = {
-      colorscheme = "catppuccin",
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
     },
   },
   {
-    "catppuccin/nvim",
-    name = "catppuccin",
+    "LazyVim/LazyVim",
     opts = {
-      transparent_background = true,
-      float = {
-        transparent = true,
-        solid = true,
-      },
-      flavour = "auto",
-      dim_inactive = {
-        enabled = true,
-        shade = "dark",
-        percentage = 0.15,
-        auto_integrations = true,
-      },
+      colorscheme = "tokyonight",
     },
   },
 }
