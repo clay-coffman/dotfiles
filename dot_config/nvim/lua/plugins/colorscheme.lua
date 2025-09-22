5a95a103d2d0e597518f1124c77423b7f33f7ee2
Clay Coffman <claymcoffman@gmail.com>
2025-09-22 11:09:05 -0700

    Update .config/kitty/kitty.conf Update .config/nvim/lua/plugins/colorscheme.lua

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -2,22 +2,15 @@ return {
   {
     "folke/tokyonight.nvim",
     opts = {
+      style = "moon",
+
       on_highlights = function(hl, c)
         -- Make all diagnostic virtual text italic
         hl.DiagnosticVirtualTextError = { fg = c.error, italic = true }
         hl.DiagnosticVirtualTextWarn = { fg = c.warning, italic = true }
         hl.DiagnosticVirtualTextInfo = { fg = c.info, italic = true }
         hl.DiagnosticVirtualTextHint = { fg = c.hint, italic = true }
-
-        hl.WinSeparator = { fg = c.blue, bold = true }
       end,
-      transparent = true,
-      styles = {
-        sidebars = "transparent",
-        floats = "transparent",
-      },
-      dim_inactive = true, -- Add this line to dim inactive windows
-      colorscheme = "moon",
     },
   },
   {
