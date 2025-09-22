874264cfbaa6d270fe3737f555c153aa253538d3
Clay Coffman <claymcoffman@gmail.com>
2025-09-22 15:03:39 -0700

    Update .config/kitty/kitty.conf Update .config/nvim/lua/plugins/colorscheme.lua

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -12,15 +12,60 @@ return {
         enabled = true,
         functions = { bold = true },
       },
+      -- Fix transparent backgrounds
+      transparent = {
+        enabled = false,
+        normal = false,
+        normalfloat = false,
+        neotree = false,
+        nvimtree = false,
+        whichkey = false,
+        telescope = false,
+        lazy = false,
+      },
     },
     config = function(plugin, opts)
       vim.o.background = "light"
       require(plugin.main).setup(opts)
       vim.cmd.colorscheme = "solarized"
+
+      -- Fix mini.files and other UI backgrounds after colorscheme loads
+      local colors = vim.g.solarized_palette
+      if colors then
+        local bg = colors.base3 -- Light mode background
+        local bg_alt = colors.base2 -- Slightly darker for contrast
+
+        -- Fix mini.files background
+        vim.cmd(string.format(
+          [[
+          highlight MiniFilesNormal guibg=%s
+          highlight MiniFilesBorder guibg=%s
+          highlight MiniFilesTitle guibg=%s
+          highlight MiniFilesTitleFocused guibg=%s
+          highlight MiniFilesCursorLine guibg=%s
+        ]],
+          bg,
+          bg,
+          bg,
+          bg,
+          bg_alt
+        ))
+
+        -- Fix other floating windows
+        vim.cmd(string.format(
+          [[
+          highlight NormalFloat guibg=%s
+          highlight FloatBorder guibg=%s guifg=%s
+          highlight WinSeparator guibg=NONE guifg=%s
+        ]],
+          bg,
+          bg,
+          bg_alt,
+          bg_alt
+        ))
+      end
     end,
   },
-
-  -- configure LazyVim to load solarized
   {
     "LazyVim/LazyVim",
     opts = {
