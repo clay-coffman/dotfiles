7cd0e911f181e70b0e8c2e4006417bae41d29611
Clay Coffman <claymcoffman@gmail.com>
2025-09-22 14:52:54 -0700

    Update .config/nvim/lua/plugins/colorscheme.lua Add .config/nvim/lua/plugins/tokyonight_colorscheme.bak Update .config/tmux/themes/solarized/dark.conf Update .config/tmux/themes/solarized/light.conf Change attributes of .tmux.conf

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,36 +1,30 @@
 return {
+  -- add Lua-scriptable solarized color scheme
   {
-    "folke/tokyonight.nvim",
+    "maxmx03/solarized.nvim",
+    branch = "main",
+    lazy = false,
+    name = "solarized",
+    main = "solarized",
+    priority = 1000,
     opts = {
-      style = "moon",
-      light_style = "day",
       styles = {
-        -- Style to be applied to different syntax groups
-        -- Value is any valid attr-list value for `:help nvim_set_hl`
-        comments = { italic = true },
-        keywords = { italic = true },
-        functions = {},
-        variables = {},
-        -- Background styles. Can be "dark", "transparent" or "normal"
-        sidebars = "normal", -- style for sidebars, see below
-        floats = "transparent", -- style for floating windows
+        enabled = true,
+        functions = { bold = true },
       },
-      day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
-      dim_inactive = true, -- dims inactive windows
-      lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
-      -- on_highlights = function(hl, c)
-      --   -- Make all diagnostic virtual text italic
-      --   hl.DiagnosticVirtualTextError = { fg = c.error, italic = true }
-      --   hl.DiagnosticVirtualTextWarn = { fg = c.warning, italic = true }
-      --   hl.DiagnosticVirtualTextInfo = { fg = c.info, italic = true }
-      --   hl.DiagnosticVirtualTextHint = { fg = c.hint, italic = true }
-      -- end,
     },
+    config = function(plugin, opts)
+      vim.o.background = "light"
+      require(plugin.main).setup(opts)
+      vim.cmd.colorscheme = "solarized"
+    end,
   },
+
+  -- configure LazyVim to load solarized
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "tokyonight",
+      colorscheme = "solarized",
     },
   },
 }
