5f66da4a5e8f81ab5b452fa18cb0faf72d2773c8
Clay Coffman <claymcoffman@gmail.com>
2026-04-24 17:25:59 +0000

    Add json formatter to conform.nvim config

--- dot_config/nvim/lua/plugins/conform.lua
+++ dot_config/nvim/lua/plugins/conform.lua
@@ -12,6 +12,7 @@ return {
         typescriptreact = { "prettierd", "prettier", stop_after_first = true },
         sql = { "sqlfluff" },
         htmldjango = { "djlint" },
+        json = { "prettierd" },
       },
     },
   },
